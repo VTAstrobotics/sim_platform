002b578d758557d4473af59a68912235f53a327f
LeScooot <jeronimus.ryan@gmail.com>
2025-09-19 18:14:09 -0400

    adding ukf launching and config file, placeholder configs

---
+++ src/ukf_launch/launch/ukf.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+
+# For retrieving a file in your package
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    package_share_dir = get_package_share_directory('ukf_launch')
+    
+
+    ukf_config_file = os.path.join(package_share_dir, 'config', 'ukf_config.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ukf_node',
+            name='ukf_node',
+            output='screen',
+            parameters=[ukf_config_file]
+        )
+
+    ])
