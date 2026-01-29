0cdbe4313e487a953517f6de62628c7c89d55bb0
LeScooot <jeronimus.ryan@gmail.com>
2026-01-28 19:06:47 -0500

    should work?

--- src/waypoint_translator/src/waypoint_follower.cpp
+++ src/waypoint_translator/src/waypoint_follower.cpp
@@ -7,7 +7,10 @@
 #include "std_msgs/msg/string.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 
+#include "rclcpp_action/rclcpp_action.hpp"
+
 #include "robot_localization/srv/from_ll.hpp"
+#include "nav2_msgs/action/navigate_to_pose.hpp"
 
 using std::placeholders::_1;
 
@@ -21,6 +24,9 @@ public:
             "/gps_waypoint", 10, std::bind(&WaypointSubscriber::gps_waypoint_callback, this, _1));
 
         from_ll_client = this->create_client<robot_localization::srv::FromLL>("/fromLL");
+
+        nav_client =
+            rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "navigate_to_pose");
     }
 
 private:
@@ -35,8 +41,6 @@ private:
         auto future =
             from_ll_client->async_send_request(request, std::bind(&WaypointSubscriber::from_ll_response_callback, this, _1));
     }
-    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_subscription;
-    rclcpp::Client<robot_localization::srv::FromLL>::SharedPtr from_ll_client;
 
     void from_ll_response_callback(rclcpp::Client<robot_localization::srv::FromLL>::SharedFuture future)
     {
@@ -44,8 +48,64 @@ private:
         const auto &p = response->map_point;
         RCLCPP_INFO(this->get_logger(), "GPS to map: x=%.3f y=%.3f z=%.3f", p.x, p.y, p.z);
 
-        //TODO Send cartesian coordinates to nav2 action
+        // TODO Send cartesian coordinates to nav2 action
+
+
+        nav2_msgs::action::NavigateToPose::Goal goal;
+        goal.pose.header.frame_id = "map";
+        goal.pose.header.stamp = this->now();
+        goal.pose.pose.position.x = p.x;
+        goal.pose.pose.position.y = p.y;
+        goal.pose.pose.position.z = 0.0;
+        goal.pose.pose.orientation.w = 1.0; 
+
+        rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SendGoalOptions options;
+
+        options.goal_response_callback =
+            [this](rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr goal_handle)
+        {
+            if (!goal_handle)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Goal rejected by Nav2");
+            }
+            else
+            {
+                RCLCPP_INFO(this->get_logger(), "Goal accepted by Nav2");
+            }
+        };
+
+        // options.feedback_callback =
+        //     [this](rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::SharedPtr,
+        //            const std::shared_ptr<const nav2_msgs::action::nav2_msgs::action::NavigateToPose::Feedback> feedback)
+        // {
+            
+        // };
+
+        options.result_callback =
+            [this](const rclcpp_action::ClientGoalHandle<nav2_msgs::action::NavigateToPose>::WrappedResult &result)
+        {
+            switch (result.code)
+            {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                RCLCPP_INFO(this->get_logger(), "Nav2 reached goal");
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_ERROR(this->get_logger(), "Nav2 aborted");
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_WARN(this->get_logger(), "Nav2 canceled");
+                break;
+            default:
+                RCLCPP_ERROR(this->get_logger(), "Unknown result code");
+                break;
+            }
+        };
+        nav_client->async_send_goal(goal, options);
     }
+
+    rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr gps_subscription;
+    rclcpp::Client<robot_localization::srv::FromLL>::SharedPtr from_ll_client;
+    rclcpp_action::Client<nav2_msgs::action::NavigateToPose>::SharedPtr nav_client;
 };
 
 int main(int argc, char *argv[])
