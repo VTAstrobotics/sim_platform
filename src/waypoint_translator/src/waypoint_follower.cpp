ddbebe1a8149b3b564802ce51d87b60d7a529365
Flame875 <awalls03@vt.edu>
2026-03-03 23:48:04 -0500

    First draft of the out of bounds code, will test tommorow

--- src/waypoint_translator/src/waypoint_follower.cpp
+++ src/waypoint_translator/src/waypoint_follower.cpp
@@ -11,6 +11,8 @@
 
 #include "robot_localization/srv/from_ll.hpp"
 #include "nav2_msgs/action/navigate_to_pose.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+
 using std::placeholders::_1;
 
 class WaypointSubscriber : public rclcpp::Node
@@ -26,11 +28,21 @@ public:
 
         nav_client =
             rclcpp_action::create_client<nav2_msgs::action::NavigateToPose>(this, "/navigate_to_pose");
+
+        pose_sub = this->create_subscription<geometry_msgs::msg::PoseWithCovarianceStamped>("/odom", 10,[this](geometry_msgs::msg::PoseWithCovarianceStamped::SharedPtr msg)
+            {
+                current_pose = msg->pose.pose;
+                pose_received = true;
+            });
     }
 
 private:
     void gps_waypoint_callback(sensor_msgs::msg::NavSatFix::SharedPtr msg)
     {
+
+        rclcpp::Subscription<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_sub;
+        geometry_msgs::msg::Pose current_pose
+        bool pose_received = false;
 
         auto request = std::make_shared<robot_localization::srv::FromLL::Request>();
         request->ll_point.latitude = msg->latitude;
@@ -55,11 +67,36 @@ private:
 
         // TODO Send cartesian coordinates to nav2 action
 
+        // Out of bounds checking starts here
+        if (!pose_received)
+        {
+            RCLCPP_WARN(this->get_logger(), "Sim does not have /odom yet, or can't find it");
+            return;
+        }
+
+        double dx = p.x - current_pose.position.x;
+        double dy = p.y - current_pose.position.y;
+        double dist = std::sqrt((dx * dx) + (dy * dy));
+
+        double max_distance = 1; // This should be the max costmap radius, maybe slightly smaller? (small size rn to test it)
+
+        double target_x = p.x;
+        double target_y = p.y;
+
+        if (dist > max_distance)
+        {
+            double scale = max_distance / dist;
+            target_x = current_pose_.position.x + dx * scale;
+            target_y = current_pose_.position.y + dy * scale;
+
+            RCLCPP_WARN(this->get_logger(), "Pose too far, moving it to %.2f m.", max_distance);
+        }
+
         nav2_msgs::action::NavigateToPose::Goal goal;
         goal.pose.header.frame_id = "map";
         goal.pose.header.stamp = this->now();
-        goal.pose.pose.position.x = p.x;
-        goal.pose.pose.position.y = p.y;
+        goal.pose.pose.position.x = target_x;
+        goal.pose.pose.position.y = target_y;
         goal.pose.pose.position.z = 0.0;
         goal.pose.pose.orientation.w = 1.0;
 
